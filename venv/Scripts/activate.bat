dc160aa18746c6559b1f692535d21dae5a41347e
ATTVN2023 <attvn2023@gmail.com>
2023-11-23 15:06:46 +0700

    fix

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\FU studying\s9\IAP491\Code\CVEWeb\venv
+set VIRTUAL_ENV=C:\FU studying\s9\IAP491\Code\CVEATTCK\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
