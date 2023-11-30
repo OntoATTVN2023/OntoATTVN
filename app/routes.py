49df3697daf1abdd9040c6996be8fe372e363f98
ATTVN2023 <149670614+ATTVN2023@users.noreply.github.com>
2023-11-30 13:35:13 +0700

    Update routes.py
    update defense

--- app/routes.py
+++ app/routes.py
@@ -6,25 +6,23 @@ from rdflib.plugins.sparql.processor import prepareQuery
 from app.owl2vowl import convert
 from app.getCVE import get_cve_info
 
-g = Graph()
-g.parse("ontology/Ontology.owl", format="xml")
+
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        cve_id = request.form['cveId'].strip()
-
-        cve_description, cwe = get_cve_info(cve_id.upper())
-        if not cve_description and not cwe:
-            return render_template('index.html', results=None, msg=f'No results found for \"{cve_id}\"', dataGraph={})
-        if not cwe:
-            cwe = {'Undefined': ''}
-            return render_template('index.html', results={'Undefined': ['Undefined']}, cve_id=cve_id.upper(), cve_description=cve_description,
+	if request.method == 'POST':
+		cve_id = request.form['cveId'].strip()
+		cve_description, cwe = get_cve_info(cve_id.upper())
+		if not cve_description and not cwe:
+			return render_template('index.html', results=None, msg=f'No results found for \"{cve_id}\"', dataGraph={})
+		if not cwe:
+			cwe = {'Undefined': ''}
+			return render_template('index.html', results={'Undefined': ['Undefined']}, cve_id=cve_id.upper(), cve_description=cve_description,
                                    cwe=cwe, dataGraph={})
-        initial_cwe_ids = set(cwe.keys())
+		initial_cwe_ids = set(cwe.keys())
 
-        list_to_filter = set([
+		list_to_filter = set([
             'CWE-6', 'CWE-15', 'CWE-20', 'CWE-46', 'CWE-59', 'CWE-73', 'CWE-74', 'CWE-94', 'CWE-95', 'CWE-96', 'CWE-97', 'CWE-113',
             'CWE-114', 'CWE-117', 'CWE-162', 'CWE-172', 'CWE-173', 'CWE-180', 'CWE-181', 'CWE-200', 'CWE-204', 'CWE-205', 'CWE-208',
             'CWE-226', 'CWE-257', 'CWE-261', 'CWE-262', 'CWE-263', 'CWE-267', 'CWE-269', 'CWE-270', 'CWE-272', 'CWE-276', 'CWE-282',
@@ -41,186 +39,133 @@ def index():
             'CWE-1323', 'CWE-1325', 'CWE-1326', 'CWE-1327', 'CWE-1330'
         ])
 
-        filtered_cwe_ids = initial_cwe_ids & list_to_filter
-        if not filtered_cwe_ids:
-            return render_template('index.html', results={'Undefined': ['Undefined']}, cve_id=cve_id.upper(), cve_description=cve_description,
+		filtered_cwe_ids = initial_cwe_ids & list_to_filter
+		if not filtered_cwe_ids:
+			return render_template('index.html', results={'Undefined': ['Undefined']}, cve_id=cve_id.upper(), cve_description=cve_description,
                                    cwe=cwe, dataGraph={})
 
-        if len(filtered_cwe_ids) > 1:
-            cwe_id = "$|".join(filtered_cwe_ids)+"$"
-        else:
-            cwe_id = filtered_cwe_ids.pop()+"$"
-
-        sparql_query = '''
-        PREFIX my: <http://test.org/Ontology.owl#>
+		if len(filtered_cwe_ids) > 1:
+			cwe_id = "$|".join(filtered_cwe_ids)+"$"
+		else:
+			cwe_id = filtered_cwe_ids.pop()+"$"
+		g = Graph()
+		g.parse("ontology/Ontology.owl", format="xml")
+		sparql_query = '''
+		PREFIX my: <http://test.org/Ontology.owl#>
 		SELECT DISTINCT ?TacticID ?TacticName ?TechID ?TechName
 		WHERE {
-            filter REGEX(?CWEID,"''' + cwe_id + '''").
-            ?CWE rdf:type my:CWE.
-            ?CWE my:hasID ?CWEID.
-            ?CWE my:hasCAPEC ?Capec.
-            ?Capec my:mapToCAPEC ?Tech.
-            ?Tech my:hasID ?TechID.
-            ?Tech my:hasName ?TechName.
-            ?Tech my:accomplishedTactic ?Tactic.
-            ?Tactic my:hasID ?TacticID.
-            ?Tactic my:hasName ?TacticName.
+		    filter REGEX(?CWEID,"''' + cwe_id + '''").
+		    ?CWE rdf:type my:CWE.
+		    ?CWE my:hasID ?CWEID.
+		    ?CWE my:hasCAPEC ?Capec.
+		    ?Capec my:mapToCAPEC ?Tech.
+		    ?Tech my:hasID ?TechID.
+		    ?Tech my:hasName ?TechName.
+		    ?Tech my:accomplishedTactic ?Tactic.
+		    ?Tactic my:hasID ?TacticID.
+		    ?Tactic my:hasName ?TacticName.
 		}'''
-        query = prepareQuery(sparql_query)
+		query = prepareQuery(sparql_query)
 
-        result = g.query(query)
-        result_data = defaultdict(list)
+		result = g.query(query)
+		result_data = defaultdict(list)
 
-        for row in result:
-            tactic = str(row["TacticName"])
-            tech = str(row["TechName"])
-            tacticId = str(row["TacticID"])
-            techId = str(row["TechID"])
-            value = f"{techId}: {tech}"
-            tactic = f"{tacticId}: {tactic}"
-            result_data[tactic].append(value)
+		for row in result:
+			tactic = str(row["TacticName"])
+			tech = str(row["TechName"])
+			tacticId = str(row["TacticID"])
+			techId = str(row["TechID"])
+			value = f"{techId}: {tech}"
+			tactic = f"{tacticId}: {tactic}"
+			result_data[tactic].append(value)
 	
-        result_dict = dict(result_data)
-        print(result_dict)
-        tactics = ['Reconnaissance', 'Resource Development', 'Initial Access', 'Execution', 'Persistence', 'Privilege Escalation', 'Defense Evasion',
+		result_dict = dict(result_data)
+		tactics = ['Reconnaissance', 'Resource Development', 'Initial Access', 'Execution', 'Persistence', 'Privilege Escalation', 'Defense Evasion',
                    'Credential Access', 'Discovery', 'Lateral Movement', 'Collection', 'Command and Control', 'Exfiltration', 'Impact']
 
-        dataGraph = dict()
-        for key, value in result_dict.items():
-            dataGraph[key.split(": ")[1]] = len(value)
-        for tactic in tactics:
-            if tactic not in dataGraph.keys():
-                dataGraph[tactic] = 0
-
-        return render_template('index.html', results=result_dict, cve_id=cve_id.upper(), cve_description=cve_description,
+		dataGraph = dict()
+		for key, value in result_dict.items():
+			dataGraph[key.split(": ")[1]] = len(value)
+		for tactic in tactics:
+			if tactic not in dataGraph.keys():
+				dataGraph[tactic] = 0
+		return render_template('index.html', results=result_dict, cve_id=cve_id.upper(), cve_description=cve_description,
                                cwe=cwe,  dataGraph=dataGraph)
 
-    else:
-        return render_template('index.html', results=None, dataGraph={})
+	else:
+		return render_template('index.html', results=None, dataGraph={})
 
 @app.route('/ontology-model')
 def ontoModel():
-    convert()
-    return render_template('ontology-model.html')
+	convert()
+	return render_template('ontology-model.html')
 
 @app.route('/defense', methods=['GET', 'POST'])
 def defense():
-    if request.method == 'POST':
-        tech_id = request.form['techID']
+	if request.method == 'POST':
+		tech_id = request.form['techID'].upper()
+		tech_id = '^'+str(tech_id)+'$'
+		g = Graph()
+		g.parse("ontoweb/OntoWeb.owl", format="xml")
 
-        sparql_query = """
-        PREFIX attack: <http://test.org/Ontology.owl#>
+		sparql_query = '''
+		PREFIX attack: <http://test.org/Ontology.owl#>
+		SELECT DISTINCT ?TechID ?TechName ?TechDescription ?DefenseID ?DefenseName ?DefenseDescription ?Type 
+		WHERE {	
+		
+			filter REGEX(?TechID,"''' + tech_id + '''").
+			?Tech rdf:type attack:Technique.
+			?Tech attack:hasID ?TechID.
+			?Tech attack:hasName ?TechName.
+			?Tech attack:hasDescription ?TechDescription.
+			?Tech attack:defenseBy ?Defense.
+			?Defense attack:hasID ?DefenseID.
+			?Defense attack:hasDescription ?DefenseDescription.
+			?Defense attack:hasName ?DefenseName.
+			?Defense attack:hasType ?Type.
+		} order by ?Type
+		'''
+		query = prepareQuery(sparql_query)
+		results = g.query(query)
+		data = {
+		    'Tech': [],
+		    'Deceive': [],
+		    'Detect': [],
+		    'Evict': [],
+		    'Model': [],
+		    'Harden': [],
+		    'Isolate': [],
+		    'Restore': []
+		}
+		tech_detail = []
+		for row in results:
+			tech_id = row['TechID']
+			tech_name = row['TechName']
+			tech_desciption = row['TechDescription']
+			defense_id = row['DefenseID']
+			defense_name = row['DefenseName']
+			defense_description = row['DefenseDescription']
+			defense_type = str(row['Type'])
+			
+			data[defense_type].append({
+				'DefenseID': str(defense_id),
+				'DefenseName': str(defense_name),
+				'DefenseDescription': str(defense_description),
+				'DefenseType': defense_type
+			    })
+		tech_detail.append({
+			'TechID': str(tech_id),
+			'TechName': str(tech_name),
+			'TechDescription': str(tech_desciption)
+			})
+		deceive_list = data['Deceive']
+		detect_list = data['Detect']
+		evict_list = data['Evict']
+		model_list = data['Model']
+		harden_list = data['Harden']
+		isolate_list = data['Isolate']
+		restore_list = data['Restore']
+		print(tech_detail)
+		return render_template('defense.html',tech_detail=tech_detail, deceive_list=deceive_list, detect_list=detect_list, evict_list=evict_list, model_list=model_list, harden_list=harden_list, isolate_list=isolate_list, restore_list=restore_list)
 
-	SELECT DISTINCT ?TechID ?TechName ?DefenseID ?DefenseName ?DefenseDescription ?Type 
-	WHERE {	
-	{
-		filter REGEX(?TechID,"T1001$").
-		?Tech rdf:type attack:Technique.
-		?Tech attack:hasID ?TechID.
-		?Tech attack:hasName ?TechName.
-		?Tech attack:mayBeDetectedBy ?Detect.
-		?Detect rdf:type attack:Detect.
-		?Detect attack:hasID ?DefenseID.
-		?Detect attack:hasDescription ?DefenseDescription.
-		?Detect attack:hasName ?DefenseName.
-		?Detect attack:hasType ?Type.
-	}union 
-	{
-		filter REGEX(?TechID,"T1001$").
-		?Tech rdf:type attack:Technique.
-		?Tech attack:hasID ?TechID.
-		?Tech attack:hasName ?TechName.
-		?Tech attack:mayBeDeceivedBy ?Deceive.
-		?Detect rdf:type attack:Deceive.
-		?Deceive attack:hasID ?DefenseID.
-		?Deceive attack:hasDescription ?DefenseDescription.
-		?Deceive attack:hasName ?DefenseName.
-		?Detect attack:hasType ?Type.
-	}union
-	{
-		filter REGEX(?TechID,"T1001$").
-		?Tech rdf:type attack:Technique.
-		?Tech attack:hasID ?TechID.
-		?Tech attack:hasName ?TechName.
-		?Tech attack:mayBeModeledBy ?Model.
-		?Detect rdf:type attack:Model.
-		?Model attack:hasID ?DefenseID.
-		?Model attack:hasDescription ?DefenseDescription.
-		?Model attack:hasName ?DefenseName.
-		?Detect attack:hasType ?Type.
-	}union
-	{
-		filter REGEX(?TechID,"T1001$").
-		?Tech rdf:type attack:Technique.
-		?Tech attack:hasID ?TechID.
-		?Tech attack:hasName ?TechName.
-		?Tech attack:mayBeHardenedBy ?Harden.
-		?Detect rdf:type attack:Harden.
-		?Harden attack:hasID ?DefenseID.
-		?Harden attack:hasDescription ?DefenseDescription.
-		?Harden attack:hasName ?DefenseName.
-		?Detect attack:hasType ?Type.
-	}union
-	{
-		filter REGEX(?TechID,"T1001$").
-		?Tech rdf:type attack:Technique.
-		?Tech attack:hasID ?TechID.
-		?Tech attack:hasName ?TechName.
-		?Tech attack:mayBeIsolatedBy ?Isolate.
-		?Detect rdf:type attack:Isolate.
-		?Isolate attack:hasID ?DefenseID.
-		?Isolate attack:hasDescription ?DefenseDescription.
-		?Isolate attack:hasName ?DefenseName.
-		?Detect attack:hasType ?Type.
-	}union
-	{
-		filter REGEX(?TechID,"T1001$").
-		?Tech rdf:type attack:Technique.
-		?Tech attack:hasID ?TechID.
-		?Tech attack:hasName ?TechName.
-		?Tech attack:mayBeEvictedBy ?Evict.
-		?Detect rdf:type attack:Evict.
-		?Evict attack:hasID ?DefenseID.
-		?Evict attack:hasDescription ?DefenseDescription.
-		?Evict attack:hasName ?DefenseName.
-		?Detect attack:hasType ?Type.	
-	}union
-	{
-		filter REGEX(?TechID,"T1001$").
-		?Tech rdf:type attack:Technique.
-		?Tech attack:hasID ?TechID.
-		?Tech attack:hasName ?TechName.
-		?Tech attack:mayBeRestoredBy ?Restore.
-		?Detect rdf:type attack:Restore.
-		?Restore attack:hasID ?DefenseID.
-		?Restore attack:hasDescription ?DefenseDescription.
-		?Restore attack:hasName ?DefenseName.
-		?Detect attack:hasType ?Type.
-	}
-	}
-        """
-        query = prepareQuery(sparql_query)
-
-        results = g.query(query)
-
-        data = []
-        for row in results:
-            tech_id = row['TechID']
-            
-            tech_name = row['TechName']
-            defense_id = row['DefenseID']
-            defense_name = row['DefenseName']
-            defense_description = row['DefenseDescription']
-            defense_type = row['Type']
-
-            data.append({
-                'TechID': str(tech_id),
-                'TechName': str(tech_name),
-                'DefenseID': str(defense_id),
-                'DefenseName': str(defense_name),
-                'DefenseDescription': str(defense_description),
-                'DefenseType': str(defense_type)
-            })
-        return render_template('defense.html', results=data)
-
-    return render_template('defense.html')
+	return render_template('defense.html')
